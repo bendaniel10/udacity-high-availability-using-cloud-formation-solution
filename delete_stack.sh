2a7c16b025258830fa2df3126583847b40cf4d93
Daniel Ben <daniel.ben@numberfour.eu>
2020-09-02 17:54:41 +0200

    updated scripts

---
+++ delete_stack.sh
@@ -0,0 +1,2 @@
+export AWS_PROFILE=udacity_user
+aws cloudformation delete-stack --stack-name $1
---
+++ update_stack.sh
@@ -0,0 +1,2 @@
+export AWS_PROFILE=udacity_user
+aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=eu-central-1
